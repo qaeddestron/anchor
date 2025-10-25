1dd86eafab54e21273e7436bc13cfb9a6ac1686c
Mogaji Qaed <mogajiqaed@gmail.com>
2025-10-25 13:57:31 +0100

    Update NGNA.sol

--- contracts/NGNA.sol
+++ contracts/NGNA.sol
@@ -1,1 +1,11 @@
+```solidity
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SNGN is ERC20 {
+    constructor() ERC20("Stabil Naira", "SNGN") {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+}
