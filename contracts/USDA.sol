ac7e6f71a87e51ab1d6d280418464e6bb8a5b614
Mogaji Qaed <mogajiqaed@gmail.com>
2025-10-25 14:02:19 +0100

    Update USDA.sol

--- contracts/USDA.sol
+++ contracts/USDA.sol
@@ -1,1 +1,11 @@
+```solidity
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDA is ERC20 {
+    constructor() ERC20("Anchor US Dollar", "USDA") {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+}
