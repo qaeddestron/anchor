a36fe31e7e129a461cc76b73a9251d10061168e3
Mogaji Qaed <mogajiqaed@gmail.com>
2025-10-25 14:03:17 +0100

    Rename SNGN contract to NGNA

--- contracts/NGNA.sol
+++ contracts/NGNA.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SNGN is ERC20 {
+contract NGNA is ERC20 {
     constructor() ERC20("Anchor Naira", "NGNA") {
         _mint(msg.sender, 1000000 * 10**decimals());
     }
